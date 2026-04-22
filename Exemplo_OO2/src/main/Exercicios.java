a160736015841cc265d084409aef9d3fde45f4fe
Zittko <daniel.y.saiki@gmail.com>
2026-04-22 20:28:00 -0300

    Aula 9

--- Exemplo_OO2/src/main/Exercicios.java
+++ Exemplo_OO2/src/main/Exercicios.java
@@ -22,7 +22,7 @@ public class Exercicios {
         }
     }
     
-    public static void exe1 () {
+    public static void exe3() {
         String show = "";
         for (int c = 1; c <= 100; c++) {
             if (c % 3 == 0) {
@@ -32,48 +32,33 @@ public class Exercicios {
         JOptionPane.showMessageDialog(null, show, "MULTIPLOS DE 3", 1);
     }
     
-    public static void exe2() {
-        String x = JOptionPane.showInputDialog(null, "Digite o 1° valor");
-        Double n1 = Double.parseDouble(x);
-        x = JOptionPane.showInputDialog(null, "Digite o 2° valor");
-        Double n2 = Double.parseDouble(x);
-        x = JOptionPane.showInputDialog(null, "Digite o 3° valor");
-        Double n3 = Double.parseDouble(x);
-        String show = "";
-        if (n1 < n2 && n1 < n3) {
-            show += "Menor número: "+n1+"\n";
-            if (n2 < n3) {
-                show += "Número médio: "+n2+"\n";
-                show += "Maior número: "+n3+"\n";
-            } else {
-                show += "Número médio: "+n3+"\n";
-                show += "Maior número: "+n2+"\n";
+    public static void exe4() {
+        int a = 0, b = 0, c = 0, t = 0;
+        String x = "";
+        
+        x = JOptionPane.showInputDialog(null, "Digite o valor 1");
+        a = Integer.parseInt(x);
+        x = JOptionPane.showInputDialog(null, "Digite o valor 1");
+        b = Integer.parseInt(x);
+        x = JOptionPane.showInputDialog(null, "Digite o valor 1");
+        c = Integer.parseInt(x);
+        
+        for (int cont = 1; cont <= 3; cont++) {
+            if (a > b) {
+                t = a;
+                a = b;
+                b = c;
+            } else if (b > c) {
+                t = b;
+                b = c;
+                c = t;
             }
-            JOptionPane.showMessageDialog(null, show, "ORDENAGEM", 1);
-        } else if (n2 < n1 && n2 < n3) {
-            show += "Menor número: "+n2+"\n";
-            if (n1 < n3) {
-                show += "Número médio: "+n1+"\n";
-                show += "Maior número: "+n3+"\n";
-            } else {
-                show += "Número médio: "+n3+"\n";
-                show += "Maior número: "+n1+"\n";
-            }
-            JOptionPane.showMessageDialog(null, show, "ORDENAGEM", 1);
-        } else {
-            show += "Menor número: "+n3+"\n";
-            if (n1 < n2) {
-                show += "Número médio: "+n1+"\n";
-                show += "Maior número: "+n2+"\n";
-            } else {
-                show += "Número médio: "+n2+"\n";
-                show += "Maior número: "+n1+"\n";
-            }
-            JOptionPane.showMessageDialog(null, show, "ORDENAGEM", 1);
         }
+        
+        JOptionPane.showMessageDialog(null, a+"/n"+b+"/n"+c);
     }
     
-    public static void exe3() {
+    public static void exe5() {
         String show = "";
         for (int i = 7; i < 200; i = i + 7) {
             show = show + i + "\n";
@@ -82,7 +67,7 @@ public class Exercicios {
         JOptionPane.showMessageDialog(null, show, "MULTIPLOS DE 7 MENORES QUE 200",1);
     }
     
-    public static void exe4() {
+    public static void exe6() {
         String show = "";
         for (int ct = 1; ct < 100; ct++) {
             if (ct % 4 == 0) {
@@ -92,7 +77,7 @@ public class Exercicios {
         JOptionPane.showMessageDialog(null, show, "MULTIPLOS DE 4 MENORES QUE 100", 1);
     }
     
-    public static void exe5() {
+    public static void exe7() {
         String x = JOptionPane.showInputDialog(null, "digite um número");
         double n = Double.parseDouble(x);
         String show = "";
@@ -102,5 +87,18 @@ public class Exercicios {
             }
         }
         JOptionPane.showMessageDialog(null, show, "Divisores de "+ n, 1);
+    }
+    
+    public static void exe8() {
+        String x = "";
+        double soma = 0, media;
+        int num;
+        for (int i = 1; i <= 10; i++) {
+            x = JOptionPane.showInputDialog(null, "Digite a idade do aluno " + i);
+            num = Integer.parseInt(x);
+            soma += num;
+        }
+        media = (soma / 10);
+        JOptionPane.showMessageDialog(null, "A média de idade dos 10 alunos é: "+media, "MEDIA", -1);
     }
 }
--- Exemplo_OO2/src/main/Main.java
+++ Exemplo_OO2/src/main/Main.java
@@ -11,6 +11,6 @@ package main;
  */
 public class Main {
     public static void main(String[] args) {
-        Exercicios.exe5();
+        Exercicios.exe6();
     }
 }
