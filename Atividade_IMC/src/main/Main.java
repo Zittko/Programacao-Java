d9ea49acb087f8d4ff7afc7fdadc2054d10de272
Zittko <daniel.y.saiki@gmail.com>
2026-05-20 20:24:41 -0300

    Aula 10

---
+++ Atividade_IMC/src/main/Main.java
@@ -0,0 +1,13 @@
+
+package main;
+
+import View.Inicio_GUI;
+import View.Segunda_GUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Segunda_GUI().setVisible(true);
+    }
+    
+}
