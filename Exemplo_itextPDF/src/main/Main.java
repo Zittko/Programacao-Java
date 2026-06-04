f95bf1946fc8a6251cf08a3d8667932131854a28
Zittko <daniel.y.saiki@gmail.com>
2026-06-04 01:40:17 -0300

    pdf

--- Exemplo_itextPDF/src/main/Main.java
+++ Exemplo_itextPDF/src/main/Main.java
@@ -25,7 +25,7 @@ public class Main {
         OutputStream os = null;
         LocalDate localDate = LocalDate.now();//For reference
         LocalTime localTime = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LL yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd 'de' MMMM 'de' yyyy");
         DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd/LL/yyyy");
         String data1 = localDate.format(formatter);
         String data2 = localDate.format(formatter2);
@@ -37,7 +37,7 @@ public class Main {
             doc = new Document(PageSize.A4, 36, 36, 36, 36);
 
             //cria a stream de saída
-            os = new FileOutputStream("C://Users//Laboratorio-Info//Documents//Programacao-Java//out.pdf");
+            os = new FileOutputStream("C://Users//reido//OneDrive//Documents//Programacao-Java//out.pdf");
 
             //associa a stream de saída ao
             PdfWriter.getInstance(doc, os);
@@ -45,32 +45,43 @@ public class Main {
             //abre o documento
             doc.open();
             Font f1 = new Font(FontFamily.HELVETICA, 30, Font.BOLD);
-            Font f2 = new Font(FontFamily.UNDEFINED, 16, Font.BOLD);
-            Font brSpacing = new Font(FontFamily.UNDEFINED, 20, Font.BOLD);
+            Font f2 = new Font(FontFamily.UNDEFINED, 14, Font.BOLD);
+            Font brSpacing = new Font(FontFamily.UNDEFINED, 10, Font.BOLD);
             //adiciona o texto ao PDF
             String doutor = JOptionPane.showInputDialog(null, "DIGITE O NOME DO(A) MEDICO(A)");
             String valorDias = JOptionPane.showInputDialog(null, "QUANTOS DIAS O(A) PACIENTE FICARÁ AUSENTE?");
             int dias = Integer.parseInt(valorDias);
-            Paragraph par = new Paragraph("ATESTADO MÉDICO", f1);
-            Paragraph line = new Paragraph("_____________________________________________________________________________");
-            par.setAlignment(Element.ALIGN_CENTER);
-            Image img = Image.getInstance("C://Users//Laboratorio-Info//Documents//Programacao-Java//Exemplo_itextPDF//src//main//hapvida.png");
-            img.setAbsolutePosition(0, 750);
-            Paragraph p2 = new Paragraph("  Atesto que atendi nesta data o(a) Sr(a) " + doutor.toUpperCase() + " às " + hora + ", sendo necessário seu afastamento do local de trabalho ou escola por " + dias +" dia(s), a partir de " + data2 + ", tendo como causa do atendimento o código abaixo:", f2);
+            Paragraph titulo = new Paragraph("ATESTADO MÉDICO", f1);
+            titulo.setAlignment(Element.ALIGN_CENTER);
+            Image imgLogo = Image.getInstance("C://Users//reido//OneDrive//Documents//Programacao-Java//Exemplo_itextPDF//src//main//hapvida.png");
+            Image imgAss = Image.getInstance("C://Users//reido//OneDrive//Documents//Programacao-Java//Exemplo_itextPDF//src//main//assinatura.png");
+            imgLogo.setAbsolutePosition(0, 750);
+            imgAss.setAbsolutePosition(180, 340);
+            Paragraph par = new Paragraph("  Atesto que atendi nesta data o(a) Sr(a) " + doutor.toUpperCase() + " às " + hora + ", sendo necessário seu afastamento do local de trabalho ou escola por " + dias +" dia(s), a partir de " + data2 + ", tendo como causa do atendimento o código abaixo:", f2);
             Paragraph br = new Paragraph("      ", brSpacing);
-            Paragraph cod = new Paragraph("CID 10-J11");
-            Paragraph dot = new Paragraph(".............................");
-            doc.add(img);
+            Paragraph pLocaData = new Paragraph("SÂO PAULO, "+ data1 + ".");
+            Paragraph assMed = new Paragraph("Assinatura do Médico", f2);
+            Paragraph linhaAss = new Paragraph("__________________________________");
+            
+            linhaAss.setAlignment(Element.ALIGN_CENTER);
+            assMed.setAlignment(Element.ALIGN_CENTER);
+            pLocaData.setAlignment(Element.ALIGN_CENTER);
+            doc.add(imgLogo);
+            doc.add(titulo);
+            doc.add(new Paragraph("_____________________________________________________________________________"));
+            for (int i = 1; i <= 4; i++) {doc.add(br);}
             doc.add(par);
-            doc.add(line);
+            for (int i = 1; i <= 2; i++) {doc.add(br);}
+            doc.add(new Paragraph("CID 10-J11"));
+            doc.add(new Paragraph("............................."));
+            doc.add(new Paragraph("Codigo da Doença", f2));
             doc.add(br);
+            doc.add(pLocaData);
             doc.add(br);
-            doc.add(br);
-            doc.add(p2);
-            doc.add(br);
-            doc.add(br);
-            doc.add(cod);
-            doc.add(dot);
+            doc.add(assMed);
+            doc.add(imgAss);
+            for (int i = 1; i <= 6; i++) {doc.add(br);}
+            doc.add(linhaAss);
 
         } finally {
 
@@ -86,7 +97,7 @@ public class Main {
             }
         }
         
-        Desktop.getDesktop().open(new File("C://Users//Laboratorio-Info//Documents//Programacao-Java//out.pdf"));
+        Desktop.getDesktop().open(new File("C://Users//reido//OneDrive//Documents//Programacao-Java//out.pdf"));
     }
     
 }
